f6845ad3fa9713f1a46f35cf2cab97c8b7163774
dursunaydin <dursunayydin@gmail.com>
2025-07-02 18:41:07 +0300

    initial commit

---
+++ src/db/schema.js
@@ -0,0 +1,19 @@
+import {
+  pgTable,
+  serial,
+  varchar,
+  text,
+  timestamp,
+  integer,
+} from "drizzle-orm/pg-core";
+
+export const favoritesTable = pgTable("favorites", {
+  id: serial("id").primaryKey(),
+  userId: varchar("user_id").notNull(),
+  recipeId: integer("recipe_id").notNull(),
+  title: text("title").notNull(),
+  image: text("image").notNull(),
+  cookTime: text("cook_time"),
+  servings: text("servings"),
+  createdAt: timestamp("created_at").defaultNow(),
+});
