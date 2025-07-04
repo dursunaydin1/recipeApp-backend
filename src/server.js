cb34051f02dd368c0e19efac44216887f758ac66
dursunaydin <dursunayydin@gmail.com>
2025-07-04 17:06:38 +0300

    corn added

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
-import { job } from "./config/corn.js";
+import  job  from "./config/corn.js";
 
 const app = express();
 const PORT = ENV.PORT || 5001;
